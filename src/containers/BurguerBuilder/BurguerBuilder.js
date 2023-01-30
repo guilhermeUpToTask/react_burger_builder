1328239241d8b274dec79e35ff8a3778815b428b
guilherme <guilherme256teretelteltel@gmail.com>
2023-01-30 11:22:55 -0300

    dynamic adding ingredients implemented

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -1,21 +1,49 @@
 import React , { Component} from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/BuildControls/BuildControls';
+
+const IGREDIENT_PRICE = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
+
 class BurguerBuilder extends Component {
     state = {
         ingredients : {
-            salad: 1,
-            meat: 2,
-            bacon: 1,
-            cheese: 2,
+            salad: 0,
+            meat: 0,
+            bacon: 0,
+            cheese: 0,
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) =>{
+        const oldCount = this.state.ingredients[type];
+        const updateCount = oldCount + 1;
+        const updateIgredients = {
+            ...this.state.ingredients
         }
+        updateIgredients[type] = updateCount;
+        
+        const priceAddition = IGREDIENT_PRICE[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({ingredients: updateIgredients, totalPrice: newPrice})
+    }
+
+    removeIgredientHandler = (type) =>{
+
     }
 
     render() {
         return(
             <Auxiliary>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Burger Controls</div>
+                <BuildControls ingredientAdded={this.addIngredientHandler}/>
             </Auxiliary>
         )
     }
