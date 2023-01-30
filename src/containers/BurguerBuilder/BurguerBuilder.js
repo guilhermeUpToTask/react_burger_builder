23e1f66b5fa917f4ae13450f69b140deca1315ba
guilherme <guilherme256teretelteltel@gmail.com>
2023-01-30 09:09:50 -0300

    burgeringredients spreed dynamic implemented

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -2,10 +2,19 @@ import React , { Component} from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 class BurguerBuilder extends Component {
+    state = {
+        ingredients : {
+            salad: 1,
+            meat: 2,
+            bacon: 1,
+            cheese: 2,
+        }
+    }
+
     render() {
         return(
             <Auxiliary>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Burger Controls</div>
             </Auxiliary>
         )
